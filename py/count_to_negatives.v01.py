429e5e409eef56e843b6d57279af9251abba3359
Oluwasegun Atolagbe <oluwasegun.atolagbe@mail.citytech.cuny.edu>
2022-04-11 18:47:40 -0400

    Completed lab file: count_to_negatives js and py, function plots

---
+++ py/count_to_negatives.v01.py
@@ -0,0 +1,4 @@
+# Create a FOR LOOP that prints the numbers 10 to -10.
+
+for i  in range(10, -11, -1):
+    print(i)
---
+++ py/count_to_negatives.v02.py
@@ -0,0 +1,7 @@
+# Create a FOR LOOP that saves the numbers 10 to -10 to a list...
+# .. and then, print the list to the terminal.
+
+numbers = []
+for i  in range(10, -11, -1):
+    numbers.append(i)
+print(numbers)
